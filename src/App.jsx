62b267999eea716657b574be8c65d7f497d186d7
candylxy95 <lee.ydnac@gmail.com>
2024-10-15 22:42:35 +0800

    Added solutions

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import ZombieFighters from "./ZombieFighters";
+
+const App = () => {
+  return (
+    <div>
+      <ZombieFighters />
+    </div>
+  );
+};
+
+export default App;
